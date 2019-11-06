5d323985b64a092dfa182decc444030368df559f
Sean Duff <gatewaywebdesign18@gmail.com>
2019-11-06 08:34:13 -0600

    latest front-end styles

--- client/app.js
+++ client/app.js
@@ -4,6 +4,11 @@ import Routes from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app.css'
 import cartIcon from './images/cartIcon.png'
+import flag from './images/flag.png'
+import payment from './images/payment.png'
+import apple from './images/apple.png'
+import returnIcon from './images/return.png'
+import logo from './images/logo.png'
 
 const App = () => {
   return (
@@ -11,7 +16,10 @@ const App = () => {
       <div className="container">
         <div className="row top-navbar-row">
           <div className="col-4">
-            <h1 className="granny-grace-header">GRANNY GRACE</h1>
+            <div className="home-section">
+              <img alt="apple-logo" className="apple-logo" src={logo} />
+              <h1 className="granny-grace-header">GRANNY GRACE</h1>
+            </div>
           </div>
           <div className="col-8 my-account-parent">
             <span className="my-account">My Account</span>
@@ -29,10 +37,30 @@ const App = () => {
       <div className="row services-menu">
         <div className="container">
           <div className="row services-row">
-            <div className="col services-col">Ships Nation-Wide</div>
-            <div className="col services-col">Secure Payment System</div>
-            <div className="col services-col">High Quality Apples</div>
-            <div className="col services-col">Return Policy</div>
+            <div className="col services-col">
+              <span className="service-icon">
+                <img className="service-icon" alt="flag" src={flag} />
+              </span>Ships Nation-Wide
+            </div>
+            <div className="col services-col">
+              <span className="service-icon">
+                <img className="service-icon" alt="payment" src={payment} />
+              </span>Secure Payment System
+            </div>
+            <div className="col services-col">
+              <span className="service-icon">
+                <img className="service-icon apple" alt="apple" src={apple} />
+              </span>High Quality Apples
+            </div>
+            <div className="col services-col">
+              <span className="service-icon">
+                <img
+                  className="service-icon"
+                  alt="return-icon"
+                  src={returnIcon}
+                />
+              </span>Return Policy
+            </div>
           </div>
         </div>
       </div>
