cdb6acc1696f4d803af09801553e1d69e820a49c
Charlie <achanont@gmail.com>
2019-11-06 10:04:24 -0600

    add reviews and single review component

---
+++ client/components/all-reviews.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import singleReview from './single-review'
+
+// const allReviews = props => {
+//   return (
+//     <div>
+//       {props.reviews.map(reviewObj => {
+//         return <singleReview review={reviewObj} />
+//       })}
+//     </div>
+//   )
+// }
+
+// export default AllReviews
+
+// reviews
+//title, content, eager loading with product api
---
+++ client/components/single-review.js
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+// const singleReview = props => {
+//   return <div>hello</div>
+// }
+
+// export default singleReview
