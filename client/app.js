d40273b970ff35ba9615052a00fad0293b808f1b
Sean Duff <gatewaywebdesign18@gmail.com>
2019-11-06 11:11:43 -0600

    most recent responsive styles

--- client/app.js
+++ client/app.js
@@ -15,12 +15,10 @@ const App = () => {
     <div>
       <div className="container">
         <div className="row top-navbar-row">
-          {/* <div className="col-4 home-section"> */}
           <div className="col-md-4 col-sm-12 col-xs-12 home-section">
             <img alt="apple-logo" className="apple-logo" src={logo} />
             <h1 className="granny-grace-header">GRANNY GRACE</h1>
           </div>
-          {/* <div className="col-8 my-account-parent"> */}
           <div className="col-md-8 col-sm-12 col-xs-12 my-account-parent">
             <div className="my-account">My Account</div>
             <div className="cart-icon-div">
@@ -67,10 +65,41 @@ const App = () => {
 
       <div className="container outer-products-container">
         <div className="row">
-          <div className="col-4 needs-padding">
+          <div className="col-md-4 col-sm-12">
+            <span className="product-filters-title">Product Filters</span>
             <div className="product-filter-container">Product Filters</div>
           </div>
-          <div className="col-8 needs-padding">
+          <div className="col-md-8 col-sm-12">
+            <div className="toggle-product-styles">
+              <div className="dropdown">
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Default Sorting
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton"
+                >
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </div>
+              <div className="grid">Grid</div>
+              <div className="list">List</div>
+            </div>
             <div className="inner-products-container">Products</div>
           </div>
         </div>
