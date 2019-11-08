6bdcf8db7c522f3cd8cda31d19f076c7f4b03930
Charlie <achanont@gmail.com>
2019-11-08 14:36:19 -0600

    latest

--- client/components/update-single-product.js
+++ client/components/update-single-product.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {updateProduct} from '../store/curProduct'
+// import './update-single-product.css'
 
 const UpdateSingleProduct = class extends React.Component {
   constructor() {
@@ -9,10 +10,24 @@ const UpdateSingleProduct = class extends React.Component {
     this.state = {
       name: '',
       price: '',
-      imageUrl: ''
+      quantity: '',
+      imageUrl: '',
+      description: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log('bout to mount')
+    if (prevProps.curProduct.name !== this.props.curProduct.name) {
+      this.setState({
+        name: this.props.curProduct.name,
+        price: this.props.curProduct.price,
+        imageUrl: this.props.curProduct.imageUrl,
+        description: this.props.curProduct.description
+      })
+    }
   }
 
   handleSubmit(evt) {
@@ -27,11 +42,11 @@ const UpdateSingleProduct = class extends React.Component {
     }
     console.log('TCL: newProduct', newProduct)
     this.props.updateProduct(curProduct.id, newProduct)
-    this.setState({
-      name: '',
-      price: '',
-      imageUrl: ''
-    })
+    // this.setState({
+    //   name: '',
+    //   price: '',
+    //   imageUrl: ''
+    // })
   }
 
   handleChange(evt) {
@@ -41,33 +56,67 @@ const UpdateSingleProduct = class extends React.Component {
   }
   render() {
     return (
-      <>
-        <h4>Update:</h4>
+      <div className="update-form-container">
+        <h4 className="update-title">Update This Product</h4>
+
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input
-            name="name"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="price">Price</label>
-          <input
-            name="price"
-            type="text"
-            value={this.state.price}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="imageUrl">Image Url</label>
-          <input
-            name="imageUrl"
-            type="text"
-            value={this.state.imageUrl}
-            onChange={this.handleChange}
-          />
+          <div className="form-group">
+            <label htmlFor="name">Product Name: </label>
+            <input
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="price">Price: </label>
+            <input
+              name="price"
+              type="text"
+              value={this.state.price}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="quantity">Quantity: </label>
+            <input
+              name="quantity"
+              type="text"
+              value={this.state.quantity}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="imageUrl">Image Url: </label>
+            <input
+              name="imageUrl"
+              type="text"
+              value={this.state.imageUrl}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </div>
+
+          <br />
+          <div className="form-group">
+            <label htmlFor="description">Description: </label>
+            <input
+              name="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </div>
           <button type="submit">Submit</button>
         </form>
-      </>
+      </div>
     )
   }
 }
