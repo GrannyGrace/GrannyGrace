2655435b895d7535cc9d6bacd07bf7aca5cc428c
Sean Duff <gatewaywebdesign18@gmail.com>
2019-11-12 09:37:43 -0600

    fixed footer styling, new admin features delete and change status

--- client/components/footer.js
+++ client/components/footer.js
@@ -13,21 +13,21 @@ const Footer = () => {
     <MDBFooter color="blue" className="font-small pt-4 mt-4">
       <MDBContainer>
         <MDBRow className="footer-row">
-          <MDBCol md="4">
+          <div className="col-md-4 col-sm-12 needs-margin-bottom">
             <h5 className="footer-title">GRANNY GRACE</h5>
             <p className="footer-content">
               405 W Superior St, Chicago, IL 60654
             </p>
-          </MDBCol>
-          <MDBCol md="4" className="text-align-bottom-outer">
+          </div>
+          <div className="text-align-bottom-outer col-md-4 col-sm-12">
             <div className="text-align-bottom-inner">
               <h5 className="copyright-info">
                 &copy; {new Date().getFullYear()} Copyright:{' '}
                 <a href="https://www.MDBootstrap.com"> Granny Grace </a>
               </h5>
             </div>
-          </MDBCol>
-          <MDBCol md="4" className="social-links">
+          </div>
+          <div className="social-links col-md-4 col-sm-12">
             <h5 className="links-title">GRANNY SOCIAL</h5>
             <ul>
               <li className="list-unstyled">
@@ -46,7 +46,7 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
-          </MDBCol>
+          </div>
         </MDBRow>
       </MDBContainer>
     </MDBFooter>
