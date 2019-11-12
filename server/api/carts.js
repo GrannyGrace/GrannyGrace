ac7dd87b66a579988fc4a6ad3536ea5835ab1b4b
Alexander Zimmerman <anderzimmerman@yahoo.com>
2019-11-11 19:08:47 -0600

    working add to cart for guest users guest remove from cart still not finished

--- server/api/carts.js
+++ server/api/carts.js
@@ -2,13 +2,13 @@ const router = require('express').Router()
 const {User, Cart, Product, CartProduct} = require('../db/models')
 module.exports = router
 
-const userCheck = (req, res, next) => {
-  if (!req.user || req.user.id !== +req.params.userId) {
-    res.json('must be logged in to correct user to edit cart')
-  } else {
-    next()
-  }
-}
+// const userCheck = (req, res, next) => {
+//   if (!req.user || req.user.id !== +req.params.userId) {
+//     res.json('must be logged in to correct user to edit cart')
+//   } else {
+//     next()
+//   }
+// }
 
 router.put('/guest/:productId', async (req, res, next) => {
   try {
@@ -19,32 +19,11 @@ router.put('/guest/:productId', async (req, res, next) => {
       include: [Product]
     })
 
+    //cart retrevial
     if (+req.params.productId === 0) {
       res.send(resCart.products)
     } else {
-      const product = await Product.findByPk(+req.params.productId)
-      await resCart.addProduct(product)
-      const updatedCart = await Cart.findByPk(+resCart.id, {include: [Product]})
-      console.log('from api carts else, guest', updatedCart.products)
-      res.send([updatedCart.products])
-    }
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.put('/:userId/:productId', async (req, res, next) => {
-  try {
-    const [resCart] = await Cart.findOrCreate({
-      where: {
-        userId: +req.params.userId
-      },
-      include: [Product]
-    })
-
-    if (+req.params.productId === 0) {
-      res.send(resCart.products)
-    } else {
+      //adding to cart
       const qty = +req.body.qty
       const foundProduct = resCart.products
         .map(prod => {
@@ -54,6 +33,7 @@ router.put('/:userId/:productId', async (req, res, next) => {
           return elem.id === +req.params.productId
         })
 
+      //adding product not in cart to cart
       if (!foundProduct) {
         const product = await Product.findByPk(+req.params.productId)
         await resCart.addProduct(product)
@@ -73,6 +53,7 @@ router.put('/:userId/:productId', async (req, res, next) => {
         res.send(updatedCart.products)
       }
 
+      //adding quantity to cart of existing product
       if (foundProduct) {
         const [cartQuantity] = await CartProduct.findAll({
           where: {
@@ -104,7 +85,82 @@ router.put('/:userId/:productId', async (req, res, next) => {
   }
 })
 
-router.delete('/:userId', userCheck, async (req, res, next) => {
+router.put('/:userId/:productId', async (req, res, next) => {
+  try {
+    const [resCart] = await Cart.findOrCreate({
+      where: {
+        userId: +req.params.userId
+      },
+      include: [Product]
+    })
+
+    //cart retrieval
+    if (+req.params.productId === 0) {
+      res.send(resCart.products)
+    } else {
+      //adding to cart
+      const qty = +req.body.qty
+      const foundProduct = resCart.products
+        .map(prod => {
+          return {id: prod.id, name: prod.name}
+        })
+        .find(elem => {
+          return elem.id === +req.params.productId
+        })
+
+      //adding product not in cart to cart
+      if (!foundProduct) {
+        const product = await Product.findByPk(+req.params.productId)
+        await resCart.addProduct(product)
+        await CartProduct.update(
+          {quantity: qty},
+          {
+            where: {
+              productId: +req.params.productId,
+              cartId: +resCart.id
+            },
+            returning: true
+          }
+        )
+        const updatedCart = await Cart.findByPk(+resCart.id, {
+          include: [Product]
+        })
+        res.send(updatedCart.products)
+      }
+
+      //adding quantity to cart of existing product
+      if (foundProduct) {
+        const [cartQuantity] = await CartProduct.findAll({
+          where: {
+            productId: +req.params.productId,
+            cartId: +resCart.id
+          },
+          attributes: ['quantity']
+        })
+
+        console.log(cartQuantity.quantity)
+        const newQty = cartQuantity.quantity + qty
+        console.log('new qty', newQty)
+
+        await CartProduct.update(
+          {quantity: newQty},
+          {
+            where: {
+              productId: +req.params.productId,
+              cartId: +resCart.id
+            },
+            returning: true
+          }
+        )
+        res.send(resCart.products)
+      }
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:userId', async (req, res, next) => {
   try {
     const cart = await Cart.findOne({
       where: {userId: +req.params.userId}
@@ -114,14 +170,14 @@ router.delete('/:userId', userCheck, async (req, res, next) => {
     products.forEach(async prod => {
       await prod.removeCart(cart)
     })
-    const updated = Cart.findByPk(cart.id, {include: [Product]})
+    await Cart.findByPk(cart.id, {include: [Product]})
     res.json([])
   } catch (error) {
     next(error)
   }
 })
 
-router.delete('/:userId/:productId', userCheck, async (req, res, next) => {
+router.delete('/:userId/:productId', async (req, res, next) => {
   try {
     const cart = await Cart.findOne({
       where: {
